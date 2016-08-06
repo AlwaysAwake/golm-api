a0fbbaa693901d016081f5840726143bb459138a
lsywind3 <lsywind3@gmail.com>
2016-08-06 16:28:58 +0900

    poll history 등록

--- routes/poll_histories.js
+++ routes/poll_histories.js
@@ -12,4 +12,25 @@ router.get('/', function(req, res, next) {
   });
 });
 
+router.post('/', function(req, res, next) {
+  var pollHistory = models.poll_histories.build({
+    user_id: req.body.user_id,
+    poll_id: req.body.poll_id,
+    answer: req.body.answer,
+    comment: req.body.comment,
+  });
+
+  pollHistory.save().then(function() {
+    res.json({
+      result: 1
+    });
+  }).catch(function(e) {
+    console.log(e);
+
+    res.json({
+      result: 0
+    });
+  });
+});
+
 module.exports = router;
